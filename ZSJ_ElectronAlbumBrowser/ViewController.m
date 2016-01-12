16563111155d3f81845a793fe30a6adc9ffe1c1c
周双建 <1542100658@qq.com>
2016-01-12 22:25:54 +0800

    Initial Commit

---
+++ ZSJ_ElectronAlbumBrowser/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZSJ_ElectronAlbumBrowser
+//
+//  Created by 周双建 on 16/1/12.
+//  Copyright © 2016年 周双建. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
